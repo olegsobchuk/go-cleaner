ab2709418e22814075ad13f46ed74b90e86d9129
Oleh Sobchuk <oleg.sobchuk@gmail.com>
2024-03-26 12:08:16 +0200

    2.0.4 Add some tests

--- checker/content.go
+++ checker/content.go
@@ -8,6 +8,15 @@ import (
 
 const fileNameException = "cleaner_config.yml"
 
+// needed this implementattion for tests
+var readFileToString = func(path string) (string, error) {
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
 func IsContentContain(fileFullPath string, suspiciousContents []string) bool {
 	if strings.HasSuffix(fileFullPath, fileNameException) {
 		return false
@@ -21,6 +30,7 @@ func IsContentContain(fileFullPath string, suspiciousContents []string) bool {
 
 	fileString = strings.ToLower(fileString)
 	fileString = strings.ReplaceAll(fileString, "\x00", "")
+	log.Println(fileString)
 	for _, str := range suspiciousContents {
 		lowerStr := strings.ToLower(str)
 
@@ -29,12 +39,4 @@ func IsContentContain(fileFullPath string, suspiciousContents []string) bool {
 		}
 	}
 	return false
-}
-
-func readFileToString(path string) (string, error) {
-	b, err := os.ReadFile(path)
-	if err != nil {
-		return "", err
-	}
-	return string(b), nil
 }
--- checker/content_test.go
+++ checker/content_test.go
@@ -13,4 +13,28 @@ func TestIsContentContain(t *testing.T) {
 		assert.False(t, res, "not ignored config file")
 	})
 
+	t.Run("read file", func(t *testing.T) {
+		filePath := "path.txt"
+		suspiciousList := []string{"suspicious"}
+
+		t.Run("file contains suspicious text", func(t *testing.T) {
+			fileText := "some suspicious string"
+			readFileToString = func(_ string) (string, error) {
+				return fileText, nil
+			}
+			res := IsContentContain(filePath, suspiciousList)
+
+			assert.True(t, res, "can't find text")
+		})
+
+		t.Run("file does not contain suspicious text", func(t *testing.T) {
+			fileText := "some normal string"
+			readFileToString = func(_ string) (string, error) {
+				return fileText, nil
+			}
+			res := IsContentContain(filePath, suspiciousList)
+
+			assert.False(t, res, "can find text")
+		})
+	})
 }
